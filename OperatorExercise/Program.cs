74d58f755c9299367f7880e2bc4a54f99364e3f0
Nikolay Loboda <nick12137@gmail.com>
2023-01-31 16:58:59 -0600

    Finished first exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace OperatorExercise
+﻿namespace OperatorsInClass
 {
-    public class Program
+
+    class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+
+static void Main(string[] args)
+{
+    //For division, create two integer variable
+    var a = 17;
+    var b = 4;
+    var quotient = a / b;
+    var remainder = a % b;
+
+
+    Console.WriteLine($"{a} / {b} is {quotient} remainder {remainder}");
 }
+        }
+
+    }
+
